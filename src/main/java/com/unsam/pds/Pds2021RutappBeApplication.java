7c523503774cdc62b1bab067c32e9a6c179416d2
joniim23 <jonathan.mansi@gmail.com>
2021-05-14 16:53:27 -0300

    Proyecto back creado con springboot

---
+++ src/main/java/com/unsam/pds/Pds2021RutappBeApplication.java
@@ -0,0 +1,13 @@
+package com.unsam.pds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pds2021RutappBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pds2021RutappBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unsam/pds/Pds2021RutappBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsam.pds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pds2021RutappBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
